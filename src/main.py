d4d0b6d91d51f754c4dd2f8da599a9a77ee81562
Lorenzo Maddalena <lorenzo.maddalena1@gmail.com>
2023-05-24 13:56:21 +0000

    implemented basic UI

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
+from podcast import Podcast
+from ui import UI
 """before you start make sure that the openai api key is set to the 
 environment variable OPENAI_API"""
 
-from podcast import Podcast
 
 #user_name = input("What is your name?\n")
 user_name = "Lorenzo Maddalena"
-todays_topic = input("What is the topic you would like to discuss today?\n")
+
+
+ui = UI()
+todays_topic = ui.topic_selection()
 
 podcast = Podcast(user_name, todays_topic)
 podcast.introduce_agent()
