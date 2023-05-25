a82fb31970e381b0e73755eb0914ab88413bcdca
Lorenzo Maddalena <lorenzo.maddalena1@gmail.com>
2023-05-25 08:08:49 +0000

    implementing form for response

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 """before you start make sure that the openai api key is set to the 
 environment variable OPENAI_API"""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 from podcast import Podcast
 from ui import UI
@@ -11,6 +14,11 @@ from ui import UI
 user_name = "Lorenzo Maddalena"
 
 app = Flask(__name__)
+app.secret_key = "secret"
+
+class inputForm(FlaskForm):
+    message = StringField('message', validators=[DataRequired()])
+    submit = SubmitField("send")
 
 
 def start_podcast():
@@ -28,12 +36,22 @@ def start_podcast():
 def home_page():
     return render_template("index.html")
 
-@app.route("/podcast")
+@app.route("/podcast", methods=["GET", "POST"])
 def podcast_page():
+
+    if request.method == "POST":
+        pass
+
+
     mypodcast = start_podcast()
     mypodcast.introduce_agent()
+    form = inputForm()
     
-    return render_template("podcast.html", messages= mypodcast.expert.messages)
+    return render_template(
+        "podcast.html", 
+        messages= mypodcast.expert.messages,
+        form=form
+        )
     
 
 if __name__== "__main__":
